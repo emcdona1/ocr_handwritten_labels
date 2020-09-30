dac771814a22d7ddc60c923f9c26c87ac91b2518
kpanthi <kpanthi@neiu.edu>
2020-09-29 22:51:48 -0400

    Initial Commit 1. User can open the image 2. image would be sent to google cloud vision api 3. cloud vision api would return OCR dta 4. OCR data will be analyzed, and extra information will be deleted, and text would be sequenced by the presence of the word in the image. 5. wrong words in the text will be masked and sent to bert to get the predictions 6. masked word would be updated with the predictions 7. output will be shown to the user. The words for which ocr value is different than the predictions, will be shown in red box. 8. user can update any words, updated words would be marked with orange. 9. if ocr value and the updated value are same those words would have green boundary, which will appear when hovering.
    Next: work on classification

--- OCR_GUI_Main.py
+++ OCR_GUI_Main.py
@@ -1,98 +1,92 @@
-
-from tkinter import*
-
-from AlgorithmicMethods import RemoveDuplicates, GetSerealizedData, GetSerealizedData2
-from globalCalls import *
-from CreateOutputFrameToDisplayInfo import CreateOutputFrameToDisplayInfo
-from statusBar import*
-
+import os
 from tkinter import filedialog
-from ScrollableImage import ScrollableImage
-from initializeDataFromImage import InitializeDataFromImage
 
-from interactiveWords import MarkWordsInImage
-from applyCorrection import ApplyCorrection
-import os, io
 from google.cloud import vision
 
+from AlgorithmicMethods import RemoveDuplicates, GetSerealizedData2
+from CreateOutputFrameToDisplayInfo import CreateOutputFrameToDisplayInfo
+from ScrollableImage import ScrollableImage
+from applyCorrection import ApplyCorrection
+from globalCalls import *
+from initializeDataFromImage import InitializeDataFromImage
+from interactiveWords import MarkWordsInImage
+from statusBar import *
 
-#constants
+# constants
 
-windowWidth =794
-windowHeight =794
+windowWidth = 794
+windowHeight = 794
 imageWidth = 788
 imageHeight = 400
-totalColumns=3
-wordHovered=""
-geometry=str(windowWidth)+"x"+str(windowHeight)
+totalColumns = 3
+wordHovered = ""
+geometry = str(windowWidth) + "x" + str(windowHeight)
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'serviceAccountToken.json'
 client = vision.ImageAnnotatorClient()
-#conf=1 even if the confidence is 100% check the word if it is actual word or not
-#conf=0.90 ignore the words that have confidance more than 0.75%
+# conf=1 even if the confidence is 100% check the word if it is actual word or not
+# conf=0.90 ignore the words that have confidance more than 0.75%
 minimumConfidence = .99
 
-
-#main window
+# main window
 root = Tk()
 root.wm_geometry(geometry)
 root.resizable(0, 0)
 root.title("Classify Specimen")
 root.config(background="white")
-root.image_window=""
+root.image_window = ""
 
 textInPath = StringVar()
 userInputVal = StringVar()
 
-#menu bar
+# menu bar
 menuBar = Menu(root)
 root.config(menu=menuBar)
-#sub Menu
-subMenu=Menu(menuBar)
-menuBar.add_cascade(label="File",menu=subMenu)
-subMenu.add_command(label="Open Image", command=lambda:openImage())
+# sub Menu
+subMenu = Menu(menuBar)
+menuBar.add_cascade(label="File", menu=subMenu)
+subMenu.add_command(label="Open Image", command=lambda: openImage())
 
-#Image canvas area Row#0 both column
+# Image canvas area Row#0 both column
 imageCanvasFrame = Frame(root)
-imageCanvasFrame.grid(row=0,column=0,sticky='nsew')
-#status bar and user input Row#1 two columns
-hoverStatusFrame=Frame(root)
-hoverStatusFrame.grid(row=1,column=0, sticky='nsew')
-CreateStatusBar(hoverStatusFrame,windowWidth)
+imageCanvasFrame.grid(row=0, column=0, sticky='nsew')
+# status bar and user input Row#1 two columns
+hoverStatusFrame = Frame(root)
+hoverStatusFrame.grid(row=1, column=0, sticky='nsew')
+CreateStatusBar(hoverStatusFrame, windowWidth)
 SetStatus("\n\t\t\t  Open image file to begin !")
 #
 outputFrame = Frame(root)
-outputFrame.grid(row=2,column=0,sticky='nsew')
-CreateOutputFrameToDisplayInfo(outputFrame,windowWidth)
+outputFrame.grid(row=2, column=0, sticky='nsew')
+CreateOutputFrameToDisplayInfo(outputFrame, windowWidth)
 
-#Hover Status Area
+
+# Hover Status Area
 
 def openImage():
     imagePath = filedialog.askopenfilename(
-        filetypes=(("PNG","*.png"),("PNG","*.png"))
+        filetypes=(("PNG", "*.png"), ("PNG", "*.png"))
     )
     processNewImage(imagePath)
 
 
 def processNewImage(imagePath):
     removeOldData()
-    df=InitializeDataFromImage(imagePath, client, vision, minimumConfidence)
+    df = InitializeDataFromImage(imagePath, client, vision, minimumConfidence)
     RemoveDuplicates(df)
-    df=GetSerealizedData2(df,3)
+    df = GetSerealizedData2(df, 3)
 
-    ApplyCorrection(dfs=df,minimumConfidence=minimumConfidence)
+    ApplyCorrection(dfs=df, minimumConfidence=minimumConfidence)
 
     root.image = PhotoImage(file=imagePath)
 
     scrollableImage = ScrollableImage(imageCanvasFrame, root=root, scrollbarwidth=6, width=imageWidth,
-                                     height=imageHeight)
+                                      height=imageHeight)
     MarkWordsInImage(root, scrollableImage, df)
     setDF(d=df)
+
 
 def removeOldData():
     ClearWordStatus()
 
+
 root.mainloop()
-
-
-
-
