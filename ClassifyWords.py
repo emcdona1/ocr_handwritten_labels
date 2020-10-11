15e37e23ef6d0689e0c4ec3de6f1db37e64e7e90
kpanthi <kpanthi@neiu.edu>
2020-10-11 00:36:38 -0400

    1. Added placeholder methods to implement classification.

---
+++ ClassifyWords.py
@@ -0,0 +1,27 @@
+def detectDescription(dfs):
+    pass
+
+
+def detectCollector(dfs):
+    pass
+
+
+def detectDate(dfs):
+    pass
+
+
+def detectRegistrationNumber(dfs):
+    pass
+
+
+def detectScienteficName(dfs):
+    pass
+
+
+def autoClassifyWords(dfs):
+    print("wip: autoClassifyWords in ClassifyWords.py")
+    detectDescription(dfs)
+    detectCollector(dfs)
+    detectDate(dfs)
+    detectRegistrationNumber(dfs)
+    detectScienteficName(dfs)
