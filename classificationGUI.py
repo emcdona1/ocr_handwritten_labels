1ec13312e227f5a28d688a8c1c90fb97cd723066
kpanthi <kpanthi@neiu.edu>
2020-10-12 22:52:11 -0400

    1. menu is added to Extract tag from image file.

--- classificationGUI.py
+++ classificationGUI.py
@@ -1,8 +1,9 @@
 import os
-from tkinter import filedialog
+from tkinter import filedialog, simpledialog
 from google.cloud import vision
 
 from algorithmicMethods import removeDuplicates, getSerealizedData
+from getSectionOfImage import getTagsFromTagUrlFile, getTagsFromImageFolder, getTagByEdgeDetection, initializeTemplates
 from wordCategories import autoClassifyWords
 from outputArea import createOutputFrameToDisplayInfo, updateOutput
 from cetectWrongWords import detectWrongWords
@@ -24,10 +25,12 @@ class ClassificationApp():
         root.popUpWidth=350
         root.popUpHeight=200
         root.wordHovered = ""
+        root.destinationFolder=os.path.expanduser("~/Desktop/")+"Tags/"
         root.geometry = str(root.windowWidth) + "x" + str(root.windowHeight)
         os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'serviceAccountToken.json'
         root.client = vision.ImageAnnotatorClient()
         initializeCategories(root)
+        initializeTemplates() #initialize templates
         # conf=1 even if the confidence is 100% check the word if it is actual word or not
         # conf=0.90 ignore the words that have confidance more than 0.75%
         root.minimumConfidence = .99
@@ -44,9 +47,19 @@ class ClassificationApp():
         root.menuBar = Menu(root)
         root.config(menu=root.menuBar)
         # sub Menu
-        subMenu = Menu(root.menuBar)
-        root.menuBar.add_cascade(label="File", menu=subMenu)
-        subMenu.add_command(label="Open Image", command=lambda: openImage())
+        #file
+        smFile = Menu(root.menuBar)
+        root.menuBar.add_cascade(label="File", menu=smFile)
+        smFile.add_command(label="Open Image", command=lambda: openImage())
+
+        #ExtractTag
+        smExtractTag = Menu(root.menuBar)
+        root.menuBar.add_cascade(label="ExtractTag", menu=smExtractTag)
+        smExtractTag.add_command(label="Change Destination: "+ root.destinationFolder, command=lambda: changeDestination(0,smExtractTag))
+        smExtractTag.add_command(label="Extract Tags from Folder",command=lambda: extractFromFolder())
+        smExtractTag.add_command(label="Extract Tag from Single Image", command=lambda: extractFromImagePath())
+        smExtractTag.add_command(label="Extract Tags from Text file containing urls", command=lambda: extractFromTxtFileUrls())
+        smExtractTag.add_command(label="Extract Tags from Image url",command=lambda: extractFromImageUrl(root))
 
         # Image canvas area Row#0 both column
         root.imageCanvasFrame = Frame(root)
@@ -62,6 +75,37 @@ class ClassificationApp():
         createOutputFrameToDisplayInfo(root, root.outputFrame)
 
         #################################################################
+        def changeDestination(index,menuItem):
+            root.destinationFolder = filedialog.askdirectory()+"/"
+            menuItem.entryconfigure(index,label="Change Destination: "+ root.destinationFolder)
+            pass
+        def extractFromFolder():
+            imageSourceFolder = filedialog.askdirectory() + "/"
+            if len(imageSourceFolder)>2:
+                getTagsFromImageFolder(imageSourceFolder, root.destinationFolder, False)
+            pass
+        def extractFromImagePath():
+            singleImagePath = filedialog.askopenfilename(
+                filetypes=(("PNG", "*.png"), ("JPG", "*.jpg"))
+            )
+            if len(singleImagePath)>1:
+                filename=singleImagePath.split('/')[-1]
+                getTagByEdgeDetection(singleImagePath, os.path.join(root.destinationFolder, filename),False)
+
+            pass
+        def extractFromTxtFileUrls():
+            txtFileContainingUrls = filedialog.askopenfilename(
+                filetypes=(("TXT", "*.txt"), ("text", "*.txt"))
+            )
+            if len(txtFileContainingUrls)>0:
+                getTagsFromTagUrlFile(txtFileContainingUrls, root.destinationFolder, False)
+            pass
+
+        def extractFromImageUrl(root):
+            imageUrl = simpledialog.askstring("Input", "Enter the image URL: ",parent=root)
+            filename = imageUrl.split('/')[-1]
+            getTagByEdgeDetection(imageUrl, os.path.join(root.destinationFolder, filename), False)
+            pass
 
         def openImage():
             root.imagePath = filedialog.askopenfilename(
