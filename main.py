49db82ee12e7188d1468031053cefe7d3342ee04
kpanthi <kpanthi@neiu.edu>
2020-10-25 19:13:49 -0400

    Minor correction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from ClassificationApp_GUI.ClassificationApp_root import ClassificationApp
-from SuggestEngine.Get_SuggestEngine import GetLocalSuggestEngine
 from UnitTesting.UnitTesting import StartUnitTesting
 
 if __name__ == '__main__':
