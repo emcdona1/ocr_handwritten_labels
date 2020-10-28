ba4e6645da0cae3d4bd3ab0074fd8a2d3c524302
kpanthi <kpanthi@neiu.edu>
2020-10-27 23:17:50 -0400

    Added SQL scripts to create table, create CURD stored procedures (MYSQL)

---
+++ DatabaseSetup/CreateDb.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS PlantClassificationDB;
+CREATE DATABASE PlantClassificationDB;
+GRANT ALL PRIVILEGES ON PlantClassificationDB.* TO 'root'@'localhost';
+FLUSH PRIVILEGES;
+USE PlantClassificationDB;
