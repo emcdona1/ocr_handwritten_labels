a1e7b3b1c2cc7a8b3560b1b555799f284383c6ae
kpanthi <kpanthi@neiu.edu>
2020-11-04 22:27:52 -0500

    Added configuration file. Now using the variables from configuration file when needed.

--- SuggestEngine/SuggestEngine_Client.py
+++ SuggestEngine/SuggestEngine_Client.py
@@ -1,37 +1,29 @@
-'''would make connection to the server and send and receive the suggested word details'''
-''' max cost is determined at server side'''
 import pickle
 import socket
 from datetime import datetime
+from configparser import ConfigParser
 
-SERVER_PORT_DEFAULT = 1234
-CLIENT_PORT_DEFAULT = 1235
+configParser = ConfigParser()
+configFilePath = r'Configuration.cfg'
+configParser.read(configFilePath)
+genusSpeciesFilePath = configParser.get('SNS_SERVER', 'genusSpeciesFilePath')
+hostName_server = configParser.get('SNS_SERVER', 'host')
+portNo_server = int(configParser.get('SNS_SERVER', 'portNo'))
+bufferSize = int(configParser.get('SNS_SERVER', 'bufferSize'))
+queueSize = int(configParser.get('SNS_SERVER', 'queueSize'))
 
 
 class SuggestEngineClient:
-    bufferSize = None
-    queueSize = None
-    portNo_client = None
-    portNo_server = None
-    hostName_client = None
-    hostName_server = None
     s = None
-
-    def __init__(self, hostName=socket.gethostname(), portNo_client=CLIENT_PORT_DEFAULT,
-                 hostName_server=socket.gethostname(), portNo_server=SERVER_PORT_DEFAULT, queueSize=3, bufferSize=1024):
-        self.bufferSize = bufferSize
-        self.queueSize = queueSize
-        self.portNo_client = portNo_client
-        self.hostName_client = hostName
-        self.hostName_server = hostName_server
-        self.portNo_server = portNo_server
+    def __init__(self):
+        pass
 
     def suggest(self, word):
         try:
             self.s = socket.socket()
-            self.s.connect((self.hostName_server, self.portNo_server))
+            self.s.connect((hostName_server, portNo_server))
             self.s.send(word.encode())
-            result = pickle.loads(self.s.recv(self.portNo_client))
+            result = pickle.loads(self.s.recv(portNo_server))
             self.s.close()
             return result
         except:
