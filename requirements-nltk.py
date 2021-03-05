e69016cd894774749ca57f77a4a0f751e79e05d0
emcdona1 <emcdona1@neiu.edu>
2021-03-05 11:13:20 -0600

    Create script to setup NLTK corpora.

---
+++ requirements-nltk.py
@@ -0,0 +1,6 @@
+""" Run this script to install the necessary NLTK corpora. """
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
