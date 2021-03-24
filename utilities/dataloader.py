b873525e622b59c0d8d36266586887cb7612db6d
emcdona1 <emcdona1@neiu.edu>
2021-03-24 10:10:12 -0500

    Put image/folder file loader into dataloader.py.

--- utilities/dataloader.py
+++ utilities/dataloader.py
@@ -1,3 +1,4 @@
+import os
 
 
 def load_list_from_txt(file_path: str) -> list:
@@ -7,4 +8,18 @@ def load_list_from_txt(file_path: str) -> list:
         for item in lines:
             item = item.strip()
             results.append(item)
+    return results
+
+
+def load_file_list_from_filesystem(directory_or_file: str) -> list:
+    results = list()
+    if os.path.isdir(directory_or_file):
+        results = os.listdir(directory_or_file)
+        results = [directory_or_file + os.path.sep + filename for filename in results]
+        results = [item for item in results if not os.path.isdir(item)]  # remove subdirectories
+    elif os.path.isfile(directory_or_file):
+        results = [directory_or_file]
+    else:
+        raise FileNotFoundError('Not a valid directory or file: %s' % directory_or_file)
+
     return results
