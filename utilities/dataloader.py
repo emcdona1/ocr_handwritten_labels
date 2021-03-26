f6d91c7d453ec18d5604082d04428c75a879dd7d
emcdona1 <emcdona1@neiu.edu>
2021-03-26 11:00:08 -0500

    Load CV2 images

--- utilities/dataloader.py
+++ utilities/dataloader.py
@@ -1,5 +1,8 @@
 import os
 import pickle
+import numpy as np
+from urllib.request import urlopen
+import cv2
 
 
 def load_list_from_txt(file_path: str) -> list:
@@ -38,3 +41,20 @@ def pickle_an_object(save_directory: str, object_id: str, obj_to_pickle) -> None
     filename = object_id + '.pickle'
     with open(os.path.join(save_directory, filename), 'wb') as file:
         pickle.dump(obj_to_pickle, file)
+
+
+def open_cv2_image(image_location: str) -> np.ndarray:
+    if 'http' in image_location:
+        resp = urlopen(image_location)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image_to_draw_on = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    else:
+        image_to_draw_on = cv2.imread(image_location)
+    return image_to_draw_on
+
+
+def save_cv2_image(save_location: str, image_id: str, image_to_save: np.ndarray) -> str:
+    filename = image_id + '-annotated.jpg'
+    file_path = os.path.join(save_location, filename)
+    cv2.imwrite(file_path, image_to_save)
+    return filename
