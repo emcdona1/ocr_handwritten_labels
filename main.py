54d3075a59000e1f236cc7c5c7732f99f4622d1c
emcdona1 <emcdona1@neiu.edu>
2021-03-29 15:43:33 -0500

    Archived

--- main.py
+++
@@ -1,8 +0,0 @@
-from ClassificationApp_GUI.ClassificationApp_root import ClassificationApp
-
-if __name__ == '__main__':
-    # StartUnitTesting()
-    loop = True
-    while loop:
-        c = ClassificationApp()
-        loop = c.loop
