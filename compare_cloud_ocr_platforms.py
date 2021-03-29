a1eb4318608e299d6c42996d45f9273472327ce7
emcdona1 <emcdona1@neiu.edu>
2021-03-29 17:20:12 -0500

    Override default file saving locations.

--- compare_cloud_ocr_platforms.py
+++ compare_cloud_ocr_platforms.py
@@ -10,19 +10,21 @@ def main():
     list_of_images = load_file_list_from_filesystem(folder_or_image_file)
     gcv_processor = GCVProcessor()
     aws_processor = AWSProcessor()
-    csv_filename = os.path.join('test_results', 'aws_gcv_comparison-' + get_timestamp_for_file_saving() + '.csv')
-    if not os.path.exists('test_results'):
-        os.mkdir('test_results')
-    csv_file = open(csv_filename, 'w')
+    folder_path = os.path.join('test_results', 'cloud_compare-' + get_timestamp_for_file_saving())
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    csv_file = open(os.path.join(folder_path, 'comparison.csv'), 'w')
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(['gcv_response', 'aws_response'])
     for one_image_location in list_of_images:
         gcv_processor.load_processed_ocr_response(one_image_location)
         gcv_annotator = gcv_processor.get_image_annotator()
+        gcv_annotator.set_save_location(os.path.join(folder_path, 'gcv'))
         draw_comparison_images(gcv_processor, gcv_annotator)
 
         aws_processor.load_processed_ocr_response(one_image_location)
         aws_annotator = aws_processor.get_image_annotator()
+        aws_annotator.set_save_location(os.path.join(folder_path, 'aws'))
         draw_comparison_images(aws_processor, aws_annotator)
         csv_writer.writerow([gcv_processor.get_full_text(), aws_processor.get_full_text()])
     csv_file.close()
