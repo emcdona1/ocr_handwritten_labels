f4824c62239c3f88730b185638a2e0889dfe2d61
emcdona1 <emcdona1@neiu.edu>
2021-03-29 14:58:40 -0500

    Remove commented out methods (deprecated)

--- imageprocessor/image_processor.py
+++ imageprocessor/image_processor.py
@@ -60,38 +60,9 @@ class ImageProcessor(ABC):
     def get_list_of_lines(self) -> list:
         pass
 
-    # def process_image(self, image_path: str):
-    #     print('Processing: ' + image_path)
-    #     self.set_image_rgb(image_path)
-    #     gcv_response_object = self.initialize_ocr_data(image_path)
-    #     self.sdb = get_normalized_sequential_data_blocks(self.dataFrame)
-    #
-    #     str_gcv = ''
-    #     for block_of_words in self.sdb:
-    #         for a_word in block_of_words:
-    #             str_gcv += a_word.description + ' '
-    #
-    #     str_dc = ''
-    #     for block_of_words in self.sdb:
-    #         for a_word in block_of_words:
-    #             str_dc += a_word.description + ' '
-    #
-    #     return gcv_response_object, str_gcv, str_dc
-    #
-    # def initialize_ocr_data(self, image_path: str):
-    #     with io.open(image_path, 'rb') as image_file:
-    #         self.imageContent = image_file.read()
-    #     self.dataFrame, gcv_response_object = get_gcv_ocr_as_data_frame_from_image(self.imageContent, self.client)
-    #     return gcv_response_object
-    #
-    # def set_image_rgb(self, image_path):
-    #     if 'http' in image_path:
-    #         resp = urlopen(image_path)
-    #         image = np.asarray(bytearray(resp.read()), dtype='uint8')
-    #         img_rgb = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    #     else:
-    #         img_rgb = cv2.imread(image_path)
-    #     self.img_rgb = img_rgb
+    @abstractmethod
+    def get_full_text(self) -> str:
+        pass
 
 
 class GCVProcessor(ImageProcessor):
