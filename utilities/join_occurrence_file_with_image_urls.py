d49d526df132ac9616da804dc4e59b862645a5a8
emcdona1 <emcdona1@neiu.edu>
2021-04-14 11:09:56 -0500

    Better way to get image URLs (because it uses your authentication to get images of sheets with protected locality info).

---
+++ utilities/join_occurrence_file_with_image_urls.py
@@ -0,0 +1,37 @@
+import sys
+import os
+from zipfile import ZipFile
+import pandas as pd
+from io import BytesIO
+from copy import deepcopy
+from dataloader import save_dataframe_as_csv
+
+
+def main(zip_file_path: str):
+    occur, images = extract_occur_and_images_from_zip(zip_file_path)
+    joined_data = join_occur_and_images_information(occur, images)
+    save_directory = os.path.dirname(zip_file_path)
+    save_location = save_dataframe_as_csv(save_directory, 'occurrence_file_with_images', joined_data)
+    print('New file saved to %s.' % save_location)
+    return joined_data
+
+
+def extract_occur_and_images_from_zip(zip_file_path: str) -> (pd.DataFrame, pd.DataFrame):
+    with ZipFile(zip_file_path, 'r') as zipped_file:
+        occur_bytes = zipped_file.read('occurrences.csv')
+        occur = pd.read_csv(BytesIO(occur_bytes), encoding='ISO-8859-1')
+        images_bytes = zipped_file.read('images.csv')
+        images = pd.read_csv(BytesIO(images_bytes), encoding='ISO-8859-1')
+    return occur, images
+
+
+def join_occur_and_images_information(occur: pd.DataFrame, images: pd.DataFrame) -> pd.DataFrame:
+    joined_information = deepcopy(occur)
+    joined_information = joined_information.assign(image_url = images['identifier'])
+    return joined_information
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) > 1, 'Please provide 1 argument: a ZIP file downloaded from the Fern Portal.'
+    filepath = sys.argv[1]
+    new_df = main(filepath)
