79da78ebe7790e6090fc2c132c287083e989eb9b
emcdona1 <emcdona1@neiu.edu>
2021-04-22 13:55:32 -0500

    Add image_folder to main

--- compare_ocrs.py
+++ compare_ocrs.py
@@ -1,17 +1,23 @@
 import sys
+import os
+import numpy as np
 import prep_comparison_data
 import calculate_changes
 
 
-def main(occurrence_filepath: str, ocr_text_filepath: str):
+def main(occurrence_filepath: str, ocr_text_filepath: str, image_folder: str):
     analysis = prep_comparison_data.main(occurrence_filepath, ocr_text_filepath)
     calculate_changes.main(analysis, 150)
 
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 3, 'Provide 2 arguments: filepath for 1 occurrence' + \
+    assert len(sys.argv) == 4, 'Provide 3 arguments: filepath for 1 occurrence' + \
                               ' (can be occurrence_with_images file), ' + \
-                              'and filepath for 1 CSV file with the headers "barcode", "aws", and "gcv" to compare.'
+                              'filepath for 1 CSV file with the headers "barcode", "aws", and "gcv" to compare,' +\
+                              'and filepath of the folder of images.'
     occur_file = sys.argv[1]
     ocr_texts = sys.argv[2]
-    main(occur_file, ocr_texts)
+    images_folder = sys.argv[3]
+    # if len(sys.argv) == 4:
+
+    main(occur_file, ocr_texts, images_folder)
