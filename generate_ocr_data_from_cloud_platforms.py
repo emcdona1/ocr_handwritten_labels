0277c70ac08896058da7a533e8052072e345ac26
emcdona1 <emcdona1@neiu.edu>
2021-04-27 10:49:23 -0500

    Cleaning and refactoring for clarity (no functionality changes).

--- generate_ocr_data_from_cloud_platforms.py
+++ generate_ocr_data_from_cloud_platforms.py
@@ -2,44 +2,45 @@ import sys
 import os
 from utilities.dataloader import load_file_list_from_filesystem, get_timestamp_for_file_saving, save_dataframe_as_csv
 from utilities.dataprocessor import convert_relative_to_absolute_coordinates
-from imageprocessor.image_processor import GCVProcessor, AWSProcessor
+from imageprocessor.image_processor import ImageProcessor, GCVProcessor, AWSProcessor
 import pandas as pd
 
 
-def main(folder_or_image_path: str, generate_images=False) -> pd.DataFrame:
+def main(folder_or_image_path: str, generate_annotated_images=False) -> pd.DataFrame:
     list_of_images = load_file_list_from_filesystem(folder_or_image_path)
     processors = [GCVProcessor(), AWSProcessor()]
-    folder_path = os.path.join('test_results', 'cloud_ocr-' + get_timestamp_for_file_saving())
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
     text_comparison = pd.DataFrame(columns=['barcode', 'gcv', 'aws'])
 
+    save_folder_path = os.path.join('test_results', 'cloud_ocr-' + get_timestamp_for_file_saving())
+    if not os.path.exists(save_folder_path):
+        os.makedirs(save_folder_path)
+
     for one_image_location in list_of_images:
-        new_text_comparison = dict()
+        new_row_of_ocr_texts = dict()
         for processor in processors:
             try:
                 processor.load_image_from_file(one_image_location)
-                new_text_comparison[processor.name] = processor.get_full_text()
+                new_row_of_ocr_texts['barcode'] = processor.current_image_barcode
+                new_row_of_ocr_texts[processor.name] = processor.get_full_text()
                 successful_ocr_query = True
             except Exception as e:
-                new_text_comparison[processor.name] = 'Error during OCR: %s' % str(e)
+                new_row_of_ocr_texts[processor.name] = 'Error during OCR: %s' % str(e)
                 successful_ocr_query = False
-            new_text_comparison['barcode'] = processor.current_image_barcode
+            if generate_annotated_images and successful_ocr_query:
+                draw_comparison_image(processor, save_folder_path)
 
-            if generate_images and successful_ocr_query:
-                annotator = processor.get_image_annotator()
-                annotator.set_save_location(os.path.join(folder_path, processor.name))
-                draw_comparison_image(processor, annotator)
-                annotator.save_annotated_image_to_file()
         print('OCR gathered for %s' % one_image_location)
-        text_comparison = text_comparison.append(new_text_comparison, ignore_index=True)
+        text_comparison = text_comparison.append(new_row_of_ocr_texts, ignore_index=True)
 
-    save_location = save_dataframe_as_csv(folder_path, 'ocr_texts', text_comparison, timestamp=False)
+    save_location = save_dataframe_as_csv(save_folder_path, 'ocr_texts', text_comparison, timestamp=False)
     print('Saved to %s' % save_location)
     return text_comparison
 
 
-def draw_comparison_image(processor, annotator) -> None:
+def draw_comparison_image(processor: ImageProcessor, save_folder_path: str) -> None:
+    annotator = processor.get_image_annotator()
+    annotator.set_save_location(os.path.join(save_folder_path, processor.name))
+
     lines = processor.get_list_of_lines()
     for line in lines:
         sorted_vertices = annotator.organize_vertices(line)
@@ -47,6 +48,7 @@ def draw_comparison_image(processor, annotator) -> None:
         sorted_vertices = [convert_relative_to_absolute_coordinates(pt, height, width) for pt in sorted_vertices]
         box_color = (0, 0, 0)  # black
         annotator.draw_polygon(sorted_vertices, box_color)
+
     words = processor.get_list_of_words()
     for word in words:
         sorted_vertices = annotator.organize_vertices(word)
@@ -56,6 +58,8 @@ def draw_comparison_image(processor, annotator) -> None:
         end_color = (0, 0, 230)  # red
         annotator.draw_line(sorted_vertices[0], sorted_vertices[3], start_color, 2)
         annotator.draw_line(sorted_vertices[1], sorted_vertices[2], end_color, 2)
+
+    annotator.save_annotated_image_to_file()
 
 
 if __name__ == '__main__':
@@ -67,4 +71,4 @@ if __name__ == '__main__':
         flag_text = sys.argv[2]
         if not flag_text == 'false' or 'False':
             generate_images_flag = True
-    results = main(folder_or_image_file, generate_images=generate_images_flag)
+    results = main(folder_or_image_file, generate_annotated_images=generate_images_flag)
