617a8b7c4cba9d8e3f540cbee51c4f5ac5f31049
emcdona1 <emcdona1@neiu.edu>
2022-01-21 14:54:16 -0600

    Update timer functionality.

--- utilities/timer.py
+++ utilities/timer.py
@@ -2,26 +2,41 @@ import time
 
 
 class Timer:
-    def __init__(self, name: str):
-        self.start_time: float = time.time()
+    def __init__(self, name='generic timer'):
+        """ Creates and starts a timer.  Optional parameter gives the timer an identifying name. """
+        self.start_time = None
+        self.active = False
+        self.start()
         self.name: str = name
         self.duration: float = 0
 
-    def start(self, name) -> None:
-        if not self.start_time:
-            self.start_time = time.time()
-            print('Timer %s started.' % self.name)
+    def start(self, name=None) -> None:
+        """ Starts a timer.  Optional parameter to update the timer's name. """
+        if self.start_time:
+            print(f'Timer {self.name} already started.')
         else:
-            print('Timer %s already started' % self.name)
+            self.start_time = time.time()
+            if name:
+                self.name = name
+            print(f'Timer {self.name} started.')
+            self.active = True
 
     def stop(self) -> None:
+        """ Stops the timer and prints the results. """
         end_time = time.time()
         self.duration = end_time - self.start_time
-        print('Timer %s stopped.' % self.name)
+        self.active = False
+        self.print_results()
 
     def print_results(self) -> None:
-        print('Completed %s in %.1f seconds.' % (self.name, self.duration))
+        """ Prints the completed duration (if stopped) or the current duration (if active). """
+        if self.active:
+            print(f'Timer {self.name} is still active.  ' +
+                  f'Current duration is {time.time() - self.start_time :.1f} seconds.')
+        else:
+            print(f'Timer completed {self.name} in {self.duration:.1f} seconds.')
 
-    def clear(self) -> None:
+    def reset(self) -> None:
+        """ Resets the timer.  Does not start it again. """
         self.start_time = None
-        self.duration = None
+        self.duration = 0
