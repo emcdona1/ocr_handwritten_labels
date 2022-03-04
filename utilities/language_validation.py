3c520dc60e28d5a0b7c819806c19e248676e8f10
Sean <scullen@neiu.edu>
2022-03-04 11:36:21 -0600

    Clean up language validation and added info to read me

--- utilities/language_validation.py
+++ utilities/language_validation.py
@@ -25,9 +25,12 @@ def compare_ground_truth(ground_truth, predicted_languages: str):
     print("Comparing predictions to ground truth...")
     predicted = pd.read_csv(predicted_languages, encoding='UTF-8')
 
-    correct, incorrect = 0, 0
+    num_correct, num_incorrect = 0, 0
     correct_list, incorrect_list, prediction_list, truth_list, barcode_list = [], [], [], [], []
 
+    # prints 'Match found' when a detected barcode matches a ground truth barcode
+    # this is for instances where the ground truth occurrence file has additional labels that langauge
+    # detection was not run on
     for v in range(len(ground_truth)):
         for p in range(len(predicted)):
             if ground_truth['Barcode'][v] == predicted['Barcode'][p]:
@@ -41,14 +44,17 @@ def compare_ground_truth(ground_truth, predicted_languages: str):
                     print('Match found')
                     correct_list.append(predicted['Document Language'][p])
                     barcode_list.append(predicted['Barcode'][p])
-                    correct = correct + 1
+                    num_correct = num_correct + 1
                     break
                 else:
                     print('Match found')
                     incorrect_list.append(predicted['Document Language'][p])
                     barcode_list.append(predicted['Barcode'][p])
-                    incorrect = incorrect + 1
+                    num_incorrect = num_incorrect + 1
                     break
+
+    print(num_correct, "correct language detections")
+    print(num_incorrect, "incorrect language detections")
 
     # initialize compare dataframe
     compare_dataframe = pd.DataFrame(data=None, index=None, columns=['Barcode', 'Predicted Lan', 'True Lan',
@@ -69,7 +75,7 @@ def compare_ground_truth(ground_truth, predicted_languages: str):
 
     # save compare dataframe to CSV
     time = get_timestamp_for_file_saving()
-    # compare_dataframe.to_csv('compare_ground_truth_dataframe' + time + '.csv')
+    compare_dataframe.to_csv('compare_ground_truth_dataframe' + time + '.csv')
     print("Finished: saving to " + 'compare_ground_truth_dataframe' + time + '.csv')
 
     # generate confusion matrix labels
